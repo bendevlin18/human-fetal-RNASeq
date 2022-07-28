95cdc27fc649c3a61bafcc407eef931561ad81d9
Ben Devlin <bendevlin@Bens-MacBook-Air.local>
2022-07-28 15:45:27 -0400

    Adding mobile site v1

---
+++ fetal_seq_mobile/global.R
@@ -0,0 +1,178 @@
+
+
+######################################
+###                                ###
+###     global.R file for          ###
+###       human-fetal-seq          ###
+###          mobile                ###
+###        application             ###
+###                                ###
+######################################
+
+
+
+##### Load in necessary packages #####
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(shinydashboard)
+library(plotly)
+library(ggpubr)
+library(DT)
+library(tidyverse)
+library(shinycssloaders)
+library(shinythemes)
+library(shinyMobile)
+
+
+
+###### TISSUE TPM GENE MATRICES ######
+##brain_df <- read.csv('brain_tpm_w_exclusions.csv')
+##placenta_df <- read.csv('placenta_tpm_w_exclusions.csv')
+
+brain_tpm_df <- read.csv('brain_tpm_w_exclusions.csv')
+placenta_tpm_df <- read.csv('placenta_tpm_w_exclusions.csv')
+
+brain_genes <- brain_tpm_df$hgnc_symbol
+placenta_genes <- placenta_tpm_df$hgnc_symbol
+choice_genes <- unique(list(brain_tpm_df$hgnc_symbol, placenta_tpm_df$hgnc_symbol))
+
+##z <- t(brain_df)
+## batch through and clean colnames for the brain
+##for (cols in colnames(brain_df)) {
+  
+##  if (grepl('tpm', cols, fixed=TRUE)) {
+    
+##    colnames(brain_df)[colnames(brain_df) == cols] <- str_split(str_split(cols, 'X')[[1]][2], '_tpm')[[1]][1]
+    
+##
+##}
+
+
+##z <- t(placenta_df)
+## batch through and clean colnames for the placenta
+##for (cols in colnames(placenta_df)) {
+  
+##  if (grepl('tpm', cols, fixed=TRUE)) {
+    
+##    colnames(placenta_df)[colnames(placenta_df) == cols] <- str_split(str_split(cols, 'X')[[1]][2], '_tpm')[[1]][1]
+    
+##  }
+##}
+
+
+
+###### TISSUE AND SAMPLE METADATA #######
+
+metadata <- read.csv('human_seq_metadata.csv', fileEncoding = 'UTF-8-BOM')
+
+### prepping metadata
+metadata$sampleID <- metadata$sampleID
+for (sample in metadata$sampleID) {
+  metadata$sampleID[metadata$sampleID == sample] <- str_split(sample, "-")[[1]][2]
+}
+metadata <- metadata[!grepl('y', metadata$exclude),]
+
+
+
+corr_plot_settings1 <- list(
+  xlab(''),
+  ylab('TPM'),
+  theme_classic(),
+  theme(rect = element_rect(fill = 'transparent'), 
+        text = element_text(size = 9),
+        plot.title = element_text(size = 9, hjust = 0.5, face = "italic"),
+        plot.margin = margin(t=1, r=1, b=0, l=1, unit="cm"),
+        axis.line = element_line(size = .75),
+        axis.ticks = element_line(size = .6),
+        legend.title = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.background = element_rect(color = NA, col = 0),
+        strip.background = element_rect(color="transparent", fill="transparent", linetype="solid")))
+
+corr_plot_settings2 <- list(
+  xlab(''),
+  ylab(''),
+  theme_classic(),
+  theme(rect = element_rect(fill = 'transparent'), 
+        text = element_text(size = 9),
+        plot.title = element_text(size = 9, hjust = 0.5, face = "italic"),
+        plot.margin = margin(t=1, r=1, b=0, l=1, unit="cm"),
+        axis.line = element_line(size = .75),
+        axis.ticks = element_line(size = .6),
+        legend.title = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.background = element_rect(color = NA, col = 0),
+        strip.background = element_rect(color="transparent", fill="transparent", linetype="solid")))
+
+corr_plot_settings3 <- list(
+  xlab('Triglycerides'),
+  ylab('TPM'),
+  theme_classic(),
+  theme(rect = element_rect(fill = 'transparent'), 
+        text = element_text(size = 9),
+        plot.title = element_text(size = 9, hjust = 0.5, face = "italic"),
+        plot.margin = margin(t=1, r=1, b=0, l=1, unit="cm"),
+        axis.line = element_line(size = .75),
+        axis.ticks = element_line(size = .6),
+        legend.title = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.background = element_rect(color = NA, col = 0),
+        strip.background = element_rect(color="transparent", fill="transparent", linetype="solid")))
+
+corr_plot_settings4 <- list(
+  xlab('Triglycerides'),
+  ylab(''),
+  theme_classic(),
+  theme(rect = element_rect(fill = 'transparent'), 
+        text = element_text(size = 9),
+        plot.title = element_text(size = 9, hjust = 0.5, face = "italic"),
+        plot.margin = margin(t=1, r=1, b=0, l=1, unit="cm"),
+        axis.line = element_line(size = .75),
+        axis.ticks = element_line(size = .6),
+        legend.title = element_blank(),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.background = element_rect(color = NA, col = 0),
+        strip.background = element_rect(color="transparent", fill="transparent", linetype="solid")))
+
+##next added for Sex Diffs plots
+
+
+
+sem <- function(x) sqrt(var(x)/length(x))
+
+box_plot_theme <- theme(panel.grid.major = element_blank(), 
+                        panel.grid.minor = element_blank(),
+                        panel.background = element_rect(fill = "transparent"), 
+                        plot.background = element_rect(fill = "transparent"),
+                        panel.border = element_rect(color="black", fill="NA", size=0.75),
+                        legend.background = element_rect(fill = "transparent"),
+                        plot.title = element_text(hjust = 0.5, face = "italic", size=9),
+                        plot.margin = margin(t=.5, r=1, b=0, l=1, unit="cm"))
+
+box_plot_theme1 <- theme(panel.grid.major = element_blank(), 
+                        panel.grid.minor = element_blank(),
+                        panel.background = element_rect(fill = "transparent"), 
+                        plot.background = element_rect(fill = "transparent"),
+                        panel.border = element_rect(color="black", fill="NA", size=0.75),
+                        legend.background = element_rect(fill = "transparent"),
+                        legend.title = element_blank(),
+                        legend.position = 'none',
+                        plot.title = element_text(hjust = 0.5, face = "italic", size=9))
+
+yy <- list(title = "TPM")
+
+
---
+++ fetal_seq_mobile/ui.R
@@ -0,0 +1,53 @@
+
+
+######################################
+###                                ###
+###         ui.R file for          ###
+###       human-fetal-seq          ###
+###          mobile                ###
+###        application             ###
+###                                ###
+######################################
+
+ui <- f7Page(title="Human Fetal-Seq Mobile",
+             options=list(theme = "light", skin="ios"),
+             f7TabLayout(
+               panels=tagList(
+                 f7Panel(side="left", theme="dark", effect="reveal",
+                         f7SmartSelect('entered_genes', label = 'Search for your gene of interest', choices=NULL, type=c("popup"), smart=TRUE, multiple=FALSE))),
+                         ##selectizeInput('entered_genes', multiple = TRUE, choices = NULL, label = 'Search for gene of interest',
+                                        ##options = list(maxItems = 1, placeholder = 'Example: Cx3cr1')))),
+               navbar=f7Navbar(
+                 title="Human Fetal-Seq Mobile", left_panel=TRUE, right_panel=FALSE),
+               f7Tabs(
+                 animated=TRUE, id='tabs',
+                 f7Tab(tabName="Landing", active=TRUE, icon=f7Icon('home_fill'),
+                       column(width=12,
+                              tags$style("#landing_text{color: black; font-size: 20px;}"),
+                              htmlOutput('landing_text'))),
+                 f7Tab(tabName="Background", active=TRUE, icon = f7Icon("email"),
+                       column(width=12,
+                              htmlOutput('background_text'))),
+                 f7Tab(tabName="Correlation Plots", active=FALSE, icon=f7Icon('layers'),
+                       column(width=4,
+                              withSpinner(plotlyOutput('br_corr_plots')))),
+                 f7Tab(tabName="Sex Differences", active=FALSE, icon=f7Icon('graph_square', lib='ios'),
+                       column(width=4,
+                              withSpinner(plotlyOutput('sex_diff')))),
+                 f7Tab(tabName="Data Table", active=FALSE, icon=f7Icon('layers'),
+                       column(width=4,
+                              withSpinner(plotlyOutput('br_data_table'))))
+                       
+               )
+               )
+               )
+
+
+
+
+
+                     
+                     
+                     
+                  
+                     
